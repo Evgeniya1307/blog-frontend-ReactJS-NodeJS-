26311a2b238405558064a1df63116ac65ee5d19f
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-10-13 13:22:21 +0400

    {/*обернула роутинг*/ }

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {BrowserRouter} from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import CssBaseline from "@mui/material/CssBaseline";
+
+import "./index.scss";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./theme";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <CssBaseline />
+    <ThemeProvider theme={theme}>
+    {/*обернула роутинг*/ }
+<BrowserRouter> 
+    <App />
+    </BrowserRouter>
+    </ThemeProvider>
+  </React.StrictMode>
+);
