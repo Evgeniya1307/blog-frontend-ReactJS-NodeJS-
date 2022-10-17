701476ffb57e86c07e53f8af8004255d5ad13421
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-10-17 11:17:11 +0400

    /при первом рендере делаю запрос

--- src/pages/FullPost.jsx
+++ src/pages/FullPost.jsx
@@ -4,43 +4,46 @@ import {useParams} from 'react-router-dom'
 import { Post } from "../components/Post";
 import { Index } from "../components/AddComment";
 import { CommentsBlock } from "../components/CommentsBlock";
-import axios from "axios";
+import axios from "../axios";
+
 
 export const FullPost = () => {
-  const [data, setDate]= React.useState()//будут дата 
+  const [data, setDate]= React.useState()//хранится data
+  const [isLoading, setIsLoading] = React.useState(true);//на загрузку
   //вытаскиваю id полученный 
   const { id } = useParams();
  
 React.useEffect(()=>{
-  axios.get(`/posts/${id}`)
+  axios.get(`/posts/${id}`)//при первом рендере делаю запрос
   .then(res=>{//выполнился когда запрос сох-ла в state
-    setDate(res.data)
+    setDate(res.data);
+    setIsLoading(false);
+  })
+  .catch((err)=>{
+    console.warn(err);
+    alert('Ошибка при получении статьи');
   });
 },[])
 
+if(isLoading){//если загрузка идёт то рендерю пост,если завершена то реальную инфу
+ return <Post isLoading={isLoading} isFullPost/>
+}
+ 
   return (
     <>
       <Post
-        id={1}
-        title="Roast the code #1 | Rock Paper Scissors"
+        id={data._id}
+        title={data.title}
         imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
-        user={{
-          avatarUrl:
-            "https://res.cloudinary.com/practicaldev/image/fetch/s--uigxYVRB--/c_fill,f_auto,fl_progressive,h_50,q_auto,w_50/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/187971/a5359a24-b652-46be-8898-2c5df32aa6e0.png",
-          fullName: "Keff",
-        }}
-        createdAt={"12 июня 2022 г."}
-        viewsCount={150}
+        user={data.user}
+        createdAt={data.createdAt}
+        viewsCount={data.viewsCount}
         commentsCount={3}
-        tags={["react", "fun", "typescript"]}
+        tags={data.tags}
         isFullPost
       >
         <p>
-          Hey there! 👋 I'm starting a new series called "Roast the Code", where
-          I will share some code, and let YOU roast and improve it. There's not
-          much more to it, just be polite and constructive, this is an exercise
-          so we can all learn together. Now then, head over to the repo and
-          roast as hard as you can!!
+          {data.text}
         </p>
       </Post>
       <CommentsBlock
