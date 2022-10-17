aca2d7171112bc7db8b6a633c972ababff4984f2
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-10-17 12:39:20 +0400

    //регистрирую для reactform

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -19,22 +19,30 @@ export const Login = () => {
   });
 
   //соз-ла функцию выполняющая тогда когда хукform понял что валидация прошла корректно
+ const onSubmit =(values)=> {
+console.log(values)
+ }
   return (
     <Paper classes={{ root: styles.root }}>
       <Typography classes={{ root: styles.title }} variant="h5">
         Вход в аккаунт
       </Typography>
+      <form onSubmit={handleSubmit(onSubmit)}>{/*handleSubmit выполнит onSubmit тогда когда нижнее поля успешно  */}
       <TextField
         className={styles.field}
         label="E-Mail"
         error
         helperText="Неверно указана почта"
+        {...register('email', {required:'Укажите почту'})}//регистрирую для reactform 
         fullWidth
       />
-      <TextField className={styles.field} label="Пароль" fullWidth />
+      <TextField className={styles.field} label="Пароль" 
+      {...register('password',{required:'Укажите пароль'} )}//регистрирую для reactform 
+      fullWidth />
       <Button size="large" variant="contained" fullWidth>
         Войти
       </Button>
+      </form>
     </Paper>
   );
 };
